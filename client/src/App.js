339101b8ee27774a9454cb2d8faae0391b74b17c
MarwanElkhoudary <marwangaza@hotmail.com>
2018-11-08 14:26:18 +0200

    card component related to #33

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import CardComp from './Components/Pages/WorkExpList/Cards';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+         <CardComp />
         </header>
       </div>
     );
