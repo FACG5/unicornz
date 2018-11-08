8c867b9c089c85bd07f1c7517e86db24d0d7ec7b
donia98 <nadooshah98@gmail.com>
2018-11-08 11:32:25 +0200

    Questionnaier Page

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import BasicDetails from './Pages/Questionnaire/BasicDetails';
+import PersonalDetails1 from './Pages/Questionnaire/PersonalDetails1';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header" />
+				<BasicDetails />
+				<PersonalDetails1 />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ client/src/Components/Pages/Questionnaire/PersonalDetails1/index.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import './style.css'
+import '../style.css'
+import Select from 'react-select';
+
+const options = [
+    { value: 'Math', label: 'Math' },
+    { value: 'Science', label: 'Science' },
+    { value: 'English', label: 'English' },
+    { value: 'physics', label: 'physics' },
+    { value: 'Biology', label: 'Biology' },
+    { value: 'calculus', label: 'calculus' },
+    { value: 'history', label: 'history' },
+    { value: 'Geographically', label: 'Geographically' },
+    { value: 'technology', label: 'technology' },
+  ];
+
+class PersonalDetails1 extends Component {
+    state = {
+        selectedOption: null, 
+    }
+
+    handleChange = (selectedOption) => {
+        this.setState({ selectedOption });
+      }
+    render() {
+        const { selectedOption } = this.state; 
+        return ( 
+            <div>
+                <div className="third-section">
+                    <h2>Now we’ve got the boring stuff out of the way, let’s get to know you better</h2>
+                    <div className="six-card">
+                        <p>What subjects are you studying? Tick all that apply</p>
+                        <Select
+                            value={selectedOption}
+                            isMulti={this.handleChange}
+                            options={options}
+                        />
+                        <p>What are your favourite subjects Azara ? Tick up to 3</p>
+                        <input type="text" className ="sec1-input"/>
+                        <p>What else do you enjoy at school? Tell us as much as you can.</p>
+                        <textarea rows="5" cols="40" placeholder="Answer"></textarea>
+                        <p>What are your interests and hobbies outside of school? Tell us everything you enjoy doing.</p>
+                        <textarea rows="5" cols="40" placeholder="Answer"></textarea>
+                    </div>
+                    <div className="seventh-card">
+                        <p>If you have a copy of your CV, please upload it to your dashboard</p>
+                        <input type="file"/>
+                        <p>Can you imagine your future career? What does it look like?</p>
+                        <textarea rows="5" cols="40" placeholder="Answer"></textarea>
+                        <p>What kinds of jobs do you think will interest you?</p>
+                        <textarea rows="5" cols="40" placeholder="Answer"></textarea>
+                    </div>
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default PersonalDetails1;
