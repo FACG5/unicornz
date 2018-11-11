468e5679f9397c153715fbdd4badb0619c43f2e9
donia98 <nadooshah98@gmail.com>
2018-11-11 11:49:09 +0200

    fix eslint problem & change the place of App files #13

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
+import Questionnaier from './Pages/Questionnaire';
 import './App.css';
 
-
-
+	
 class App extends Component {
 
 	render() {
 		return (
 			<div className="App">
 				<header className="App-header" />
-
+				<Questionnaier />
 			</div>
 		);
 	}
+}
 
 export default App;
