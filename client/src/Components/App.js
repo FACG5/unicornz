4f6e3edbe52cc22b1621efbe02a7acd4345eb2c8
MarwanElkhoudary <marwangaza@hotmail.com>
2018-11-21 14:30:01 +0200

    fixed styling

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -19,7 +19,7 @@ class App extends Component {
       <div className="App">
        <Header />
       <BrowserRouter>
-       <div>
+       <div className="view-section">
          <Switch>
             <Route exact path="/" component={LandingPage} />
             <Route path="/dash" component={Dashboard}/>
