137df707130f9f88143594d72ca474148b2334dd
MarwanElkhoudary <marwangaza@hotmail.com>
2018-11-27 11:08:54 +0200

    fix problems in database

--- server/index.js
+++ server/index.js
@@ -13,47 +13,47 @@ sequelize.sync().then(() => {
   });
 
   //Fake data generation code: Delete this section later
-  school.destroy({
-    where: {
-      id: {
-        [Sequelize.Op.gt]: 6
-      }
-    }
-  }).then(() => {
-    company.destroy({
-      where: {}
-    }).then(() => {
-      girl.destroy({
-        where: {}
-      }).then(() => {
-        for (var i = 1; i <= 50; i++) {
-          girl.create({
-            user_name: "eve" + i,
-            first_name: "Eyvon" + i,
-            last_name: "fishwall",
-            email: `eve${i}@hotmail.com`,
-            school_id: (i % 2 === 0) ? 1 : 2,
-            other_school: null,
-            birthdate: '2000-01-01T00:00:00',
-            password: '$2a$10$b87/SM7GS1rjoFV2i8z5X.dGGQcZPW8l.fhVHxgg89GnOBAGLlf7a'
-          })
+  // school.destroy({
+  //   where: {
+  //     id: {
+  //       [Sequelize.Op.gt]: 6
+  //     }
+  //   }
+  // }).then(() => {
+  //   company.destroy({
+  //     where: {}
+  //   }).then(() => {
+  //     girl.destroy({
+  //       where: {}
+  //     }).then(() => {
+  //       for (var i = 1; i <= 50; i++) {
+  //         girl.create({
+  //           user_name: "eve" + i,
+  //           first_name: "Eyvon" + i,
+  //           last_name: "fishwall",
+  //           email: `eve${i}@hotmail.com`,
+  //           school_id: (i % 2 === 0) ? 1 : 2,
+  //           other_school: null,
+  //           birthdate: '2000-01-01T00:00:00',
+  //           password: '$2a$10$b87/SM7GS1rjoFV2i8z5X.dGGQcZPW8l.fhVHxgg89GnOBAGLlf7a'
+  //         })
 
-          school.create({
-            name: 'school' + i
-          });
-          company.create({
+  //         school.create({
+  //           name: 'school' + i
+  //         });
+  //         company.create({
 
-            company_name: 'company' + i,
-            mobile: '123456789-' + i,
-            website: `www.company${i}.com`,
-            email: `company${i}@hotmail.com`,
-            facebook: `www.facebook.com/company${i}`,
-            password: '$2a$10$b87/SM7GS1rjoFV2i8z5X.dGGQcZPW8l.fhVHxgg89GnOBAGLlf7a'
-          });
-        }
-      });
-    });
-  });
+  //           company_name: 'company' + i,
+  //           mobile: '123456789-' + i,
+  //           website: `www.company${i}.com`,
+  //           email: `company${i}@hotmail.com`,
+  //           facebook: `www.facebook.com/company${i}`,
+  //           password: '$2a$10$b87/SM7GS1rjoFV2i8z5X.dGGQcZPW8l.fhVHxgg89GnOBAGLlf7a'
+  //         });
+  //       }
+  //     });
+  //   });
+  // });
 //End of face data generation section : delete to this mark
 
 
