a6b2b148f8296b7ffa5e8bb8d5d451bac70fcfb2
donia98 <nadooshah98@gmail.com>
2018-12-09 13:38:44 +0200

    delete start image

--- client/src/Components/Pages/Dashboard/index.js
+++ client/src/Components/Pages/Dashboard/index.js
@@ -1,9 +1,10 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import PersonalInfo from './PersonalInfo';
 import Find from './ActiveJobs';
-import Active from './MyActivety';
-import starr from './images/star.svg';
+// import Active from './MyActivety';
 import './style.css';
 
 class Dashboard extends Component {
@@ -14,20 +15,18 @@ class Dashboard extends Component {
         <div className="dash-board">
           <div className="sub-div">
             <PersonalInfo history={this.props.history} id={id} />
-            <Find />
+
           </div>
-          <div className="sub-div" />
           <div className="sub-div">
-            <Active
+            <Find />
+            {/* <Active
               activites={[
                 { job: 'job1', done: true },
                 { job: 'job2', done: false },
                 { job: 'job3', done: true },
                 { job: 'job4', done: false },
               ]}
-            />
-
-            <img src={starr} alt="str" className="starr" />
+            /> */}
           </div>
         </div>
       </div>
