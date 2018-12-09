49269bf41ef2c123bcbaaa6fb56f3ca1de2c9c1a
MarwanElkhoudary <marwangaza@hotmail.com>
2018-12-10 00:57:46 +0200

    list of cities in united kingdom

--- client/src/Components/Pages/Dashboard/ActiveJobs/index.js
+++ client/src/Components/Pages/Dashboard/ActiveJobs/index.js
@@ -50,7 +50,64 @@ class Search extends Component {
       <div className="find">
         <h2>Find Job</h2>
         <span>Location</span>
-        <input type="text" name="location" onChange={this.handleChange} id="location-inp"/>
+        <input type="text" name="location" list="options" onChange={this.handleChange} id="location-inp"/>
+        <datalist id="options">
+           <select>
+              <option value="london"></option>
+              <option value="Bath"></option>
+              <option value="Birmingham"></option>
+              <option value="Bradford"></option>
+              <option value="Brighton & Hove"></option>
+              <option value="Bristol"></option>
+              <option value="Cambridge"></option>
+              <option value="Canterbury"></option>
+              <option value="Carlisle"></option>
+              <option value="Chelmsford"></option>
+              <option value="Chester"></option>
+              <option value="Chichester"></option>
+              <option value="Coventry"></option>
+              <option value="Derby"></option>
+              <option value="Durham"></option>
+              <option value="Ely"></option>
+              <option value="Exeter"></option>
+              <option value="Gloucester"></option>
+              <option value="Hereford"></option>
+              <option value="Kingston-upon-Hull"></option>
+              <option value="Lancaster"></option>
+              <option value="Leeds"></option>
+              <option value="Leicester"></option>
+              <option value="Lichfield"></option>
+              <option value="Lincoln"></option>
+              <option value="Liverpool"></option>
+              <option value="(City of) London"></option>
+              <option value="London"></option>
+              <option value="Manchester"></option>
+              <option value="Newcastle-upon-Tyne"></option>
+              <option value="Norwich"></option>
+              <option value="Nottingham"></option>
+              <option value="Oxford"></option>
+              <option value="Peterborough"></option>
+              <option value="Plymouth"></option>
+              <option value="Portsmouth"></option>
+              <option value="Preston"></option>
+              <option value="Ripon"></option>
+              <option value="Salford"></option>
+              <option value="Salisbury"></option>
+              <option value="Sheffield"></option>
+              <option value="Southampton"></option>
+              <option value="St Albans"></option>
+              <option value="Stoke-on-Trent"></option>
+              <option value="Sunderland"></option>
+              <option value="Truro"></option>
+              <option value="Wakefield"></option>
+              <option value="Wells"></option>
+              <option value="(City of) Westminster"></option>
+              <option value="Winchester"></option>
+              <option value="Wolverhampton"></option>
+              <option value="Worcester"></option>
+              <option value="York"></option>
+           </select>
+         </datalist>
         <span>Subject</span>
         <select name="subject" onChange={this.handleChange}>
           <option selected>All</option>
