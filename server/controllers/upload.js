68a218cbd89d265b3efc6fbf03ad2085c9bc5ccb
ahmad <ahmadtayeb91@gmail>
2018-12-12 22:24:31 +0200

    setting the upload file element in the Questionnaire

--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -19,7 +19,7 @@ exports.post = async (request, response) => {
         const result=  await girl.update({files: sequelize.fn('array_append', sequelize.col('files'), fileLinlk)},{where: {id:girlId}});
       }else {
         response.status(200).send('not authorized');
-      }response.status(200).send('Successfully uploaded ');
+      }response.status(200).send('files Successfully uploaded ');
     } else {
       response.status(404).send('failed to upload data');
     }
